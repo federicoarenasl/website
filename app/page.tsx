9f585c931a5f50b0f8a6dae4a89fbc53f51d3ffb
Federico Arenas <federicoarenas1246@gmail.com>
2025-12-26 13:44:26 -0500

    feat(website): improve introduction page.

--- app/page.tsx
+++ app/page.tsx
@@ -3,16 +3,25 @@ export default function Page() {
   return (
     <section>
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        Hi, I'm Fede!
+        Hey there, I'm Federico Arenas.
       </h1>
       <p className="mb-4">
-        {`Nice to e-meet you. I'm Federico Arenas, fullstack Software Engineer, 
-        ML Engineer, and, more broadly, AI Engineer. You will
-        find me launching innovative AI-driven products inside seed-level startups. `}
+        {`Currently Senior AI Engineer at `}
+        <a 
+          href="https://www.materiom.org"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-blue-600 dark:text-white underline decoration-white dark:decoration-white hover:text-blue-800 dark:hover:text-white visited:text-purple-600 dark:visited:text-white"
+        >
+          Materiom
+        </a>
+        {`. Leading the development of data-intensive applications that utilize AI to accelerate bio-based material innovation.`}
       </p>
       <p className="mb-4">
-        {`Currently finding balance through running, philosophy, and my guitar. 
-        I see AI as a tool to bridge gaps and push the boundaries of what’s possible.`}
+        {`Previously contributed to early-stage AI startups in the self-driving space, computer vision space, and entreprise data space.`}
+      </p>
+      <p className="mb-4">
+        {`I find balance through running, philosophy, and my guitar.`}
       </p>
       <p className="mb-4">
         {`Based in London. Feel free to reach out.`}
